5df71d2b15d888c9baec90684fa82adfd0c9bb67
Keyworkz <info@keyworkz.nl>
2024-05-20 15:53:16 +0200

    got stuck on play pause

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, { useState } from "react";
 import "./styles/app.scss";
 import Song from "./components/Song";
 import Player from "./components/Player";
+import music from "./util";
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(music());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
     </div>
   );
 }
